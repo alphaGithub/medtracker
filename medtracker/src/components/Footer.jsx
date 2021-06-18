190d41d2b03df164e60780bce15785a64932628a
alphaGithub <amit4081596kushwaha@gmail.com>
2021-06-18 10:42:54 +0530

    Express

--- medtracker/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return (
-        <footer style={{background: "rgb(161, 224, 158)",position:"fixed",width:"100%",margin:"auto",left:0,bottom:0,textAlign:"center"}}>
-            <p>copyright @ 2021</p>
-        </footer>
-    );
-}
-
-export default Footer;
--- medtracker/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header(props){
-    return (
-        <header style={{background:"#a1e09e"}}>
-            <h1 style={{marginBottom:"unset"}}>{props.appName}</h1>
-        </header>
-    );
-}
-
-export default Header;
