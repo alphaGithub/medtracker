c8f80912f41f4f9724bc5358a1a83c60b187d6bf
alphaGithub <amit4081596kushwaha@gmail.com>
2021-06-20 12:20:11 +0530

    update final

--- medtracker/src/components/Home.jsx
+++ medtracker/src/components/Home.jsx
@@ -1,16 +1,22 @@
 import Nav from "./Nav";
 import Search from "./Search";
+import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import useStyles from "../script/style";
+
 
 function Home(){
+    const classes = useStyles();
 
     return (
         <div>
         <Nav/>
+        <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <div>
+        <div className={classes.paper}>
             <Search/>
         </div>
+         </Container>
     </div>
     );
 }
