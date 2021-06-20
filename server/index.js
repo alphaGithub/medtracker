5d790e6a5ca039dd12c6cc720166ac07c52d19bf
alphaGithub <amit4081596kushwaha@gmail.com>
2021-06-20 12:23:03 +0530

    allow-origin

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
+    res.setHeader('Access-Control-Allow-Origin',process.env.FRONTEND_ADDRESS);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
